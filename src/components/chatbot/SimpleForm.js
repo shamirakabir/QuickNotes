a1d2343a25316fe07a7476379a4567f0bc8bbecc
SsanyaS14 <srisanya12@gmail.com>
2021-05-06 17:45:08 -0500

    chatbot

--- src/components/chatbot/SimpleForm.js
+++ src/components/chatbot/SimpleForm.js
@@ -6,10 +6,10 @@ import { ThemeProvider } from 'styled-components';
 const theme = {
     background: '#f5f8fb',
     fontFamily: 'Helvetica Neue',
-    headerBgColor: '#47c0de',
+    headerBgColor: '#e87217',
     headerFontColor: '#fff',
     headerFontSize: '15px',
-    botBubbleColor: '#47c0de',
+    botBubbleColor: '#e87217',
     botFontColor: '#fff',
     userBubbleColor: '#fff',
     userFontColor: '#4a4a4a',
